18d952bb793bf0f06de27b8c6d6141eec7b831e5
E190989X <mathieu.murie@etu.univ-nantes.fr>
2022-04-07 14:09:30 +0200

    style : format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import About from './pages/About';
 import Home from './pages/Home';
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>}> </Route>
-      <Route path="/about" element={<About/>}> </Route>
-      <Route path="*" element={<Home/>}> </Route> {/*404*/}
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />}> </Route>
+        <Route path="/about" element={<About />}> </Route>
+        <Route path="*" element={<Home />}> </Route> {/*404*/}
+      </Routes>
     </BrowserRouter>
   );
 };
