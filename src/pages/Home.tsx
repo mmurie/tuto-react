abf6d1ae8a697f2796af8a20fd498fb512986117
E190989X <mathieu.murie@etu.univ-nantes.fr>
2022-04-07 17:21:17 +0200

    feat : ant design form

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,29 @@ import Countries from '../components/Countries';
 import Logo from '../components/Logo';
 import Navigation from '../components/Navigation';
 import { UserContext } from '../UserContext';
+import { Form, Input, Button } from 'antd';
 
 const Home = () => {
     const { user, setUser } = useContext(UserContext);
-    const [userName, setUserName] = useState("");
+
+    const onConnect = (values: any) => {
+        const user = {
+            username: values.pseudo,
+        };
+        if (setUser !== undefined)
+            setUser(user);
+    };
+
+    const onDeconnect = (values: any) => {
+        // call logout
+        if (setUser !== undefined)
+            setUser(null);
+    };
+
+    const onFailed = (errorInfo: any) => {
+        console.log('Failed:', errorInfo);
+    };
+
 
     return (
         <div>
@@ -14,45 +33,58 @@ const Home = () => {
             <Navigation></Navigation>
             <h1>ACCUEIL</h1>
 
-
             {user ? (
                 <div>
                     <p>Bonjour {user.username} :D</p>
-                    <button
-                        onClick={() => {
-                            // call logout
-                            if (setUser !== undefined)
-                                setUser(null);
-                        }}
+
+                    <Form
+                        name="deconnexion"
+                        labelCol={{ span: 1 }}
+                        wrapperCol={{ span: 4 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onDeconnect}
+                        onFinishFailed={onFailed}
+                        autoComplete="off"
                     >
-                        Se déconnecter
-                    </button>
+                        <Form.Item
+                            wrapperCol={{ offset: 0, span: 0 }}
+                        >
+                            <Button type="primary" htmlType="submit">
+                                Se déconnecter
+                            </Button>
+                        </Form.Item>
+                    </Form>
                     <Countries></Countries>
                 </div>
             ) : (
                 <div>
-                    <p> Veuillez renseigner votre pseudo s'il vous plaît !</p>
-                    <div className='login-form'>
-                        <label htmlFor="username">Pseudo : </label>
-                        <input type="text" id="username" onChange={(e) => {
-                            setUserName((e.target.value))
-                        }} />
-                    </div>
-                    <button
-                        onClick={async () => {
-                            const user = {
-                                username: userName,
-                            };
-                            if (setUser !== undefined)
-                                setUser(user);
-                        }}
+                    <p> Veuillez renseigner votre pseudo s'il vous plaît :</p>
+                    <Form
+                        name="connexion"
+                        labelCol={{ span: 1 }}
+                        wrapperCol={{ span: 4 }}
+                        initialValues={{ remember: true }}
+                        onFinish={onConnect}
+                        onFinishFailed={onFailed}
+                        autoComplete="off"
                     >
-                        Se connecter
-                    </button>
+                        <Form.Item
+                            label="Pseudo"
+                            name="pseudo"
+                            rules={[{ required: true, message: "Veuillez renseigner votre pseudo s'il vous plaît !" }]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            wrapperCol={{ offset: 1, span: 0 }}
+                        >
+                            <Button type="primary" htmlType="submit">
+                                Se connecter
+                            </Button>
+                        </Form.Item>
+                    </Form>
                 </div>
             )}
-
-
         </div>
     );
 };
